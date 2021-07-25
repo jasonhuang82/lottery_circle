ed69201c7db7ac5612ca93ea1189f86d72f991c0
Jasonhuang82 <jasonhuang702@gmail.com>
2021-07-25 21:57:14 +0800

    feat(): add production mode run app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 const path = require('path');
 const config = {
-  distDir: "../../dist",
+  // distDir: "../../dist",
+  devIndicators: {
+    autoPrerender: true, // 不顯示prender指標
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Important: return the modified config
+
+    config.resolve = {
+      modules: [__dirname, "node_modules"],
+      alias: {
+        ...config.resolve.alias,
+        "components": path.resolve(__dirname, 'src/client/components'),
+      },
+    };
+
+    return config;
+  },
 };
 
 
